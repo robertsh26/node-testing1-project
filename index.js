90168ebfcb57630a1fb25b379d1f1b29f67543a8
Robert Shepherd <robertsh26@outlook.com>
2024-07-25 13:49:34 -0700

    main

--- index.js
+++ index.js
@@ -114,7 +114,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -131,7 +132,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if (distance <= milesCanDrive) {
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+      this.odometer = this.odometer + milesCanDrive
+      this.tank = 0
+    }
+    return this.odometer
   }
 
   /**
@@ -146,7 +155,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
@@ -162,6 +177,7 @@ class Car {
  * isEvenNumberAsync(3).then(result => {
  *    // result is false
  * })
+ * 
  */
 function isEvenNumberAsync(number) {
   // ✨ implement
